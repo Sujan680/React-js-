f4b87e3a70aa7a68e739014f89bba264d4e56a88
sujan <budhasujan3@gmail.com>
2024-12-24 19:06:38 +0545

    Initial commit of Meals API project

---
+++ meals-api-project/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MealsApi from './components/MealsApi';
+
+const App = () => {
+  return (
+    <div>
+      <MealsApi/>
+    </div>
+  )
+}
+
+export default App;
---
+++ meals-api-project/src/components/MealsApi.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../style.css';
+
+const MealsApi = () => {
+  return (
+    <div>MealsApi</div>
+  )
+}
+
+export default MealsApi;
